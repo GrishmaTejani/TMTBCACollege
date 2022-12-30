dfb3efd47dc4cb29686e57e7e5827715b129b5a7
pc <sem3b.68.tmtbca@gmail.com>
2022-12-30 10:05:46 +0530

    login commit

---
+++ app/src/main/java/com/example/tmtbcacollegeapp/Activity_stud_login.java
@@ -0,0 +1,14 @@
+package com.example.tmtbcacollegeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_stud_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stud_login);
+    }
+}
